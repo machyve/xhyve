bdc9da69517992bc8097ccf57dc0d40f610f803c
Jakub Klama <jakub@ixsystems.com>
2016-02-01 02:20:04 +0100

    Correct copyright message.

--- src/pci_virtio_9p.c
+++ src/pci_virtio_9p.c
@@ -1,5 +1,6 @@
 /*-
- * Copyright (c) 2015 iXsystems Inc.
+ * Copyright (c) 2016 Jakub Klama <jceel@FreeBSD.org>
+ * Copyright (c) 2016 iXsystems Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
